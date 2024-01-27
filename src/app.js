358271a515451e4ed861a404d72392847933fc4f
Diana PF <149829535+KitsuneDai@users.noreply.github.com>
2024-01-27 22:02:47 +0000

    Card generator with button

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const palos = ["♦", "♥", "♠", "♣"];
+const cifra = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+let arribaPalo = document.querySelector("#arribaPalo");
+let numero = document.querySelector("#numero");
+let abajoPalo = document.querySelector("#abajoPalo");
+
+function generadorCarta(palos, cifra) {
+  let paloSeleccion = Math.floor(Math.random() * palos.length);
+  let cifraSeleccion = Math.floor(Math.random() * cifra.length);
+  arribaPalo.innerText = palos[paloSeleccion];
+  abajoPalo.innerText = palos[paloSeleccion];
+  numero.innerText = cifra[cifraSeleccion];
+
+  if (paloSeleccion <= 1) {
+    arribaPalo.style.color = "red";
+    abajoPalo.style.color = "red";
+  } else {
+    arribaPalo.style.color = "black";
+    abajoPalo.style.color = "black";
+    numero.style.color = "black";
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generadorCarta(palos, cifra);
 };
+
+document.querySelector(".btn").addEventListener("click", () => {
+  generadorCarta(palos, cifra);
+});
